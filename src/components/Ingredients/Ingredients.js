2476b40bddc27d8a0d9fc9b9de4d190b816a8057
Yidah <yidahit@hotmail.com>
2021-03-31 14:05:11 +0100

    Add and remove ingredients working use of useState

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+
+const Ingredients = ()=> {
+  const [userIngredients, setUserIngredients] = useState([]);
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}])
+  }
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients => prevIngredients.filter((ingredient)=>{
+      return ingredient.id !== ingredientId
+    }));
+  }
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient ={addIngredientHandler}/>
+
+      <section>
+        <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
