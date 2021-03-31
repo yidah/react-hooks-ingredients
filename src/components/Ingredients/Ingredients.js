de21edcd6ce5b8d6644e9fab003e93f83274c43e
Yidah <yidahit@hotmail.com>
2021-03-31 17:12:36 +0100

    Used useEffect and useCallback hooks to load data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,31 +1,56 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 
-
-const Ingredients = ()=> {
+const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
-  const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}])
-  }
-  const removeIngredientHandler = ingredientId => {
-    setUserIngredients(prevIngredients => prevIngredients.filter((ingredient)=>{
-      return ingredient.id !== ingredientId
-    }));
-  }
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  }, []);
+
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      'https://react-hooks-ingredients-d4c75-default-rtdb.firebaseio.com/ingredients.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: { 'Content-type': 'application/json' },
+      }
+    ).then((response) => {
+      return response.json();
+    }).then(responseData =>{
+      // responseData.name is how Firebase calls the automatically generated id's of elements in a table
+      setUserIngredients((prevIngredients) => [
+        ...prevIngredients,
+        { id:responseData.name, ...ingredient },
+      ]);
+    });
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => {
+        return ingredient.id !== ingredientId;
+      })
+    );
+  };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient ={addIngredientHandler}/>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
+  //Object destructuring to control dependencies in useEffect
+  const {onLoadIngredients} = props;
+  const [enteredFilter, setEnteredFilter] = useState('');
+
+  useEffect(() => {
+    // preparing query as expected by Firebase
+    const query = enteredFilter.length === 0 ? '':`?orderBy="title"&equalTo="${enteredFilter}"`;
+    fetch('https://react-hooks-ingredients-d4c75-default-rtdb.firebaseio.com/ingredients.json' + query)
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngrendients = [];
+        for (const key in responseData) {
+          loadedIngrendients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        props.onLoadIngredients(loadedIngrendients);
+      });
+  }, [enteredFilter, onLoadIngredients]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredFilter}
+            onChange={(event) => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
