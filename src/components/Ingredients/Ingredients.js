94bde4c6ef0b6d2c3c62a73c79564b0d6b35b969
Yidah <yidahit@hotmail.com>
2021-04-01 09:49:00 +0100

    Delete Ingredients Handling errors and state batching

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,17 +1,21 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading]=useState(false);
+  const [error, setError]=useState();
 
-  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setUserIngredients(filteredIngredients);
   }, []);
 
   const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
     fetch(
       'https://react-hooks-ingredients-d4c75-default-rtdb.firebaseio.com/ingredients.json',
       {
@@ -19,31 +23,52 @@ const Ingredients = () => {
         body: JSON.stringify(ingredient),
         headers: { 'Content-type': 'application/json' },
       }
-    ).then((response) => {
-      return response.json();
-    }).then(responseData =>{
-      // responseData.name is how Firebase calls the automatically generated id's of elements in a table
-      setUserIngredients((prevIngredients) => [
-        ...prevIngredients,
-        { id:responseData.name, ...ingredient },
-      ]);
-    });
+    )
+      .then((response) => {
+        setIsLoading(false);
+        return response.json();
+      })
+      .then((responseData) => {
+        // responseData.name is how Firebase calls the automatically generated id's of elements in a table
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   const removeIngredientHandler = (ingredientId) => {
-    setUserIngredients((prevIngredients) =>
-      prevIngredients.filter((ingredient) => {
-        return ingredient.id !== ingredientId;
-      })
-    );
+    setIsLoading(true);
+    fetch(
+      `https://react-hooks-ingredients-d4c75-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
+      {
+        method: 'DELETE',
+      }
+    ).then((response) => {
+      setIsLoading(false);
+      setUserIngredients((prevIngredients) =>
+        prevIngredients.filter((ingredient) => {
+          return ingredient.id !== ingredientId;
+        })
+      );
+    }).catch(error=>{
+      setError('something went wrong');
+      setIsLoading(false);
+    });
   };
+
+  const clearError = () =>{
+    setError(null);
+  }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} 
+                      loading={isLoading}/>
 
       <section>
-        <Search onLoadIngredients={filteredIngredientsHandler}/>
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList
           ingredients={userIngredients}
           onRemoveItem={removeIngredientHandler}
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,30 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo((props) => {
   //Object destructuring to control dependencies in useEffect
-  const {onLoadIngredients} = props;
+  const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    // preparing query as expected by Firebase
-    const query = enteredFilter.length === 0 ? '':`?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://react-hooks-ingredients-d4c75-default-rtdb.firebaseio.com/ingredients.json' + query)
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngrendients = [];
-        for (const key in responseData) {
-          loadedIngrendients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    const timer = setTimeout(() => {
+      // enteredFilter : entered value when we set the timer
+      // inputRef.current.value : current value as it was declared outside the closure
+      if (enteredFilter === inputRef.current.value) {
+        // preparing query as expected by Firebase
+        const query =
+          enteredFilter.length === 0
+            ? ''
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(
+          'https://react-hooks-ingredients-d4c75-default-rtdb.firebaseio.com/ingredients.json' +
+            query
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngrendients = [];
+            for (const key in responseData) {
+              loadedIngrendients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            props.onLoadIngredients(loadedIngrendients);
           });
-        }
-        props.onLoadIngredients(loadedIngrendients);
-      });
-  }, [enteredFilter, onLoadIngredients]);
+      }
+    }, 500);
+    // useEffect cleanup function 
+    // this function runs BEFORE useEffect runs the next time
+    return () => {
+      // clear timer in javascript
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -32,6 +51,7 @@ const Search = React.memo((props) => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={(event) => setEnteredFilter(event.target.value)}
